59d03ecdf34b0a38947d9f2bcc6b22c7fc2a439d
tiriscef <koun@gmx.de>
2020-01-12 01:37:57 +0100

    production statistics

--- scripts/control/communication.lua
+++ scripts/control/communication.lua
@@ -2,6 +2,10 @@
 -- Communication is very important in a relationship.
 Communication = {}
 
+local global
+local item_statistics
+local fluid_statistics
+
 function Communication.create_flying_text(entry, text)
     local entity = entry[ENTITY]
 
@@ -10,6 +14,28 @@ function Communication.create_flying_text(entry, text)
         position = entity.position,
         text = text
     }
+end
+
+function Communication.log_item(item, amount)
+    item_statistics.on_flow(item, amount)
+end
+
+function Communication.log_fluid(fluid, amount)
+    fluid_statistics.on_flow(fluid, amount)
+end
+
+local function set_locals()
+    global = _ENV.global
+    item_statistics = game.forces.player.item_production_statistics
+    fluid_statistics = game.forces.player.fluid_production_statistics
+end
+
+function Communication.init()
+    set_locals()
+end
+
+function Communication.load()
+    set_locals()
 end
 
 return Communication
