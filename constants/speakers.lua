1e8dbd4276958ab01344763d2140270f399498f5
tiriscef <koun@gmx.de>
2020-01-28 14:26:37 +0100

    schedule messages

--- constants/speakers.lua
+++ constants/speakers.lua
@@ -1,15 +1,27 @@
 Speakers = {}
 
 Speakers["tiriscef."] = {
-    useless_banter_count = 10,
+    useless_banter_count = 15,
     lines_with_followup = {
-        [2] = true
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        15
     },
     index = 0
 }
 
 Speakers["profanity."] = {
     useless_banter_count = 10,
-    lines_with_followup = {},
+    lines_with_followup = {1, 7, 9},
     index = 10000
 }
+
+for _, speaker in pairs(Speakers) do
+    speaker.lines_with_followup = Tirislib_Tables.to_lookup(speaker.lines_with_followup)
+end
