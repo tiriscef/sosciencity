d1e5e6ce3fc78d9b31a11b677eac1e144e4383a7
tiriscef <koun@gmx.de>
2020-02-07 11:41:11 +0100

    filters don't work 100% of all times

--- scripts/data-updates/fawoxylas.lua
+++ scripts/data-updates/fawoxylas.lua
@@ -1,7 +1,7 @@
 for _, tree in Tirislib_Entity.pairs("tree") do
     tree:add_mining_result {
         name = "fawoxylas",
-        probability = 0.1,
+        probability = 0.5,
         amount_min = 1,
         amount_max = 5
     }
