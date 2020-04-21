374baa180afbf066ad869db1677ab78b66084972
tiriscef <koun@gmx.de>
2020-04-21 23:50:10 +0200

    nan swan fish entity

--- prototypes/fauna.lua
+++ prototypes/fauna.lua
@@ -13,6 +13,7 @@ Tirislib_Item.batch_create(animals, {subgroup = "sosciencity-fauna", stack_size 
 ---------------------------------------------------------------------------------------------------
 -- << entities >>
 -- 'fish' entity to have ducks swimming on water bodies
+-- it seems like the factorio engine treads the order-string of the autoplace definition as some kind of ID, so I'm giving them a distinct one to be sure
 Tirislib_Entity.create {
     type = "fish",
     name = "primal-quacker",
@@ -22,7 +23,7 @@ Tirislib_Entity.create {
     minable = {mining_time = 0.4, result = "primal-quacker", count = 1},
     max_health = 20,
     subgroup = "creatures",
-    order = "b-a",
+    order = "a",
     collision_box = {{-0.75, -0.75}, {0.75, 0.75}},
     selection_box = {{-0.5, -0.3}, {0.5, 0.3}},
     pictures = {
@@ -33,5 +34,34 @@ Tirislib_Entity.create {
             scale = 1. / 4.
         }
     },
-    autoplace = {influence = 0.01}
+    autoplace = {
+        order = "sosciencity-a",
+        influence = 0.01
+    }
+}
+
+Tirislib_Entity.create {
+    type = "fish",
+    name = "nan-swan",
+    icon = "__sosciencity-graphics__/graphics/icon/nan-swan.png",
+    icon_size = 64,
+    flags = {"placeable-neutral", "not-on-map"},
+    minable = {mining_time = 0.4, result = "nan-swan", count = 1},
+    max_health = 40,
+    subgroup = "creatures",
+    order = "b",
+    collision_box = {{-1, -1}, {1, 1}},
+    selection_box = {{-0.666, -0.4}, {0.666, 0.4}},
+    pictures = {
+        {
+            filename = "__sosciencity-graphics__/graphics/entity/nan-swan/nan-swan.png",
+            width = 64,
+            height = 128,
+            scale = 1. / 3.
+        }
+    },
+    autoplace = {
+        order = "sosciencity-b",
+        influence = 0.005
+    }
 }
