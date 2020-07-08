97fdfa302eb949401c86576bcda17ae806b0cd35
tiriscef <koun@gmx.de>
2020-07-09 01:57:59 +0200

    landfill

--- prototypes/fauna.lua
+++ prototypes/fauna.lua
@@ -21,24 +21,29 @@ Tirislib_Recipe.create {
     category = "sosciencity-fishery",
     energy_required = 10,
     ingredients = {},
-    results = {
-        {type = "item", name = "elder-nan", amount_min = 2, amount_max = 4}
-    },
-    subgroup = "sosciencity-fauna",
-    main_product = "elder-nan"
+    results = {},
+    icon = "__sosciencity-graphics__/graphics/icon/fishing.png",
+    icon_size = 64,
+    subgroup = "sosciencity-fauna"
 }
 
 ---------------------------------------------------------------------------------------------------
 -- << entities >>
 -- 'fish' entity to have ducks swimming on water bodies
--- it seems like the factorio engine treads the order-string of the autoplace definition as some kind of ID, so I'm giving them a distinct one to be sure
+-- it seems like the factorio engine treats the order-string of the autoplace definition as some kind of ID, so I'm giving them a distinct one to be sure
 Tirislib_Entity.create {
     type = "fish",
     name = "primal-quacker",
     icon = "__sosciencity-graphics__/graphics/icon/primal-quacker.png",
     icon_size = 64,
     flags = {"placeable-neutral", "not-on-map"},
-    minable = {mining_time = 0.4, result = "primal-quacker", count = 1},
+    minable = {
+        mining_time = 0.4,
+        results = {
+            {name = "primal-quacker", amount = 1},
+            {name = "primal-quackling", amount_min = 0, amount_max = 5}
+        }
+    },
     max_health = 20,
     subgroup = "creatures",
     order = "a",
@@ -65,7 +70,13 @@ Tirislib_Entity.create {
     icon = "__sosciencity-graphics__/graphics/icon/nan-swan.png",
     icon_size = 64,
     flags = {"placeable-neutral", "not-on-map"},
-    minable = {mining_time = 0.4, result = "nan-swan", count = 1},
+    minable = {
+        mining_time = 0.4,
+        results = {
+            {name = "nan-swan", amount = 1},
+            {name = "nan-swanling", amount_min = 0, amount_max = 3}
+        }
+    },
     max_health = 40,
     subgroup = "creatures",
     order = "b",
--- prototypes/garbage.lua
+++ prototypes/garbage.lua
@@ -13,3 +13,15 @@ Tirislib_Item.create {
     subgroup = "sosciencity-garbage",
     stack_size = 200
 }:add_sprite_variations(64, "__sosciencity-graphics__/graphics/icon/food-leftovers", 3)
+
+Tirislib_Recipe.create {
+    name = "garbage-to-landfill",
+    energy_required = 1,
+    category = "crafting",
+    ingredients = {
+        {"garbage", 20}
+    },
+    results = {
+        {"landfill", 1}
+    }
+}:add_unlock("landfill")
