31280252015222c018e4f1c2112b6b365f341bb8
tiriscef <koun@gmx.de>
2022-04-05 22:39:50 +0200

    handcrafting icon

--- datastage-scripts/handcrafting.lua
+++ datastage-scripts/handcrafting.lua
@@ -3,5 +3,7 @@ for _, recipe in Tirislib.Recipe.iterate() do
         local original_description = recipe.localised_description or {"recipe-description." .. recipe.name}
 
         recipe.localised_description = {"", original_description, "\n", {"sosciencity-util.handcrafting"}}
+
+        recipe:add_icon_layer("__sosciencity-graphics__/graphics/utility/hand.png", "topright")
     end
 end
