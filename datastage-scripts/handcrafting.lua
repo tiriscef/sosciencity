a68e7ebe73bdb4ec56f096f67f3fee9a4ff73ce6
tiriscef <koun@gmx.de>
2022-04-05 23:23:52 +0200

    burn recipes for the other garbage items

--- datastage-scripts/handcrafting.lua
+++ datastage-scripts/handcrafting.lua
@@ -4,6 +4,6 @@ for _, recipe in Tirislib.Recipe.iterate() do
 
         recipe.localised_description = {"", original_description, "\n", {"sosciencity-util.handcrafting"}}
 
-        recipe:add_icon_layer("__sosciencity-graphics__/graphics/utility/hand.png", "topright")
+        recipe:add_icon_layer("__sosciencity-graphics__/graphics/utility/hand.png", "topright", 0.25, {a = 0.5, r = 1, g = 1, b = 1})
     end
 end
