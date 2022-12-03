2bc3775e8867d2fc6a5707f9d894f476149464f0
tiriscef <koun@gmx.de>
2022-12-03 03:29:12 +0100

    remove now-obsolete migration files

--- migrations/migration_0.0.12.lua
+++
@@ -1,1 +0,0 @@
-global.notifications = global.notifications or {}
--- migrations/migration_0.0.4.lua
+++
@@ -1,9 +0,0 @@
-local WarningType = require("enums.warning-type")
-
-for _, warning_type in pairs(WarningType) do
-    if not global.warnings[warning_type] then
-        global.warnings[warning_type] = {
-            last_warning_tick = 0
-        }
-    end
-end
