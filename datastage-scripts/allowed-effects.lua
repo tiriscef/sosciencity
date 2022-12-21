f3d714b2cf6ac60b323033c62a7fc6e90e36b23a
tiriscef <koun@gmx.de>
2022-12-21 11:28:00 +0100

    not allowing productivity effects cause it's useless

--- datastage-scripts/allowed-effects.lua
+++ datastage-scripts/allowed-effects.lua
@@ -3,8 +3,5 @@ for _, entity in Tirislib.Entity.iterate {"assembling-machine", "mining-drill", 
         if not Tirislib.Tables.contains(entity.allowed_effects, "speed") then
             table.insert(entity.allowed_effects, "speed")
         end
-        if not Tirislib.Tables.contains(entity.allowed_effects, "productivity") then
-            table.insert(entity.allowed_effects, "productivity")
-        end
     end
 end
