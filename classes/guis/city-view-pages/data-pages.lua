66240e9fa02427bd9126bc245f4da8977af38f1d
tiriscef <koun@gmx.de>
2024-10-09 23:12:33 +0200

    some sortable list refactoring +that bug in the macronutrient columns

--- classes/guis/city-view-pages/data-pages.lua
+++ classes/guis/city-view-pages/data-pages.lua
@@ -19,101 +19,103 @@ local function create_food_locales()
     end
 end
 
-Gui.Elements.SortableList.linked_data["food"] = Food.values
-Gui.Elements.SortableList.linked_categories["food"] = {
-    {
-        name = "name",
-        localised_name = {"city-view.food"},
-        content = function(entry)
-            return {"", string.format("[item=%s] ", entry.name), entry.localised_name}
-        end,
-        tooltip = function(entry)
-            return entry.localised_description
-        end,
-        order = function(entry)
-            return entry.name
-        end,
-        constant_font = "default-bold"
-    },
-    {
-        name = "calories",
-        localised_name = {"sosciencity.calories"},
-        content = function(entry)
-            return {"sosciencity.value-with-unit", math.floor(entry.calories), {"sosciencity.kcal"}}
-        end,
-        order = function(entry)
-            return entry.calories
-        end
-    },
-    {
-        name = "fat",
-        localised_name = {"sosciencity.fat"},
-        content = function(entry)
-            return string.format("%.0f%%", entry.fat / entry.calories * 1000)
-        end,
-        order = function(entry)
-            return entry.fat
-        end
-    },
-    {
-        name = "carbohydrates",
-        localised_name = {"sosciencity.carbohydrates"},
-        content = function(entry)
-            return string.format("%.0f%%", entry.carbohydrates / entry.calories * 1000)
-        end,
-        order = function(entry)
-            return entry.carbohydrates
-        end
-    },
-    {
-        name = "proteins",
-        localised_name = {"sosciencity.proteins"},
-        content = function(entry)
-            return string.format("%.0f%%", entry.proteins / entry.calories * 1000)
-        end,
-        order = function(entry)
-            return entry.proteins
-        end
-    },
-    {
-        name = "healthiness",
-        localised_name = {"sosciencity.health"},
-        content = function(entry)
-            return entry.healthiness
-        end,
-        order = function(entry)
-            return entry.healthiness
-        end
-    },
-    {
-        name = "luxury",
-        localised_name = {"sosciencity.luxury"},
-        content = function(entry)
-            return entry.luxury
-        end,
-        order = function(entry)
-            return entry.luxury
-        end
-    },
-    {
-        name = "taste",
-        localised_name = {"sosciencity.tastiness"},
-        content = function(entry)
-            return entry.taste_quality
-        end,
-        order = function(entry)
-            return entry.taste_quality
-        end
-    },
-    {
-        name = "taste-category",
-        localised_name = {"sosciencity.taste"},
-        content = function(entry)
-            return Food.taste_names[entry.taste_category]
-        end,
-        order = function(entry)
-            return entry.taste_category
-        end
+Gui.Elements.SortableList.linked["food"] = {
+    data = Food.values,
+    categories = {
+        {
+            name = "name",
+            localised_name = {"city-view.food"},
+            content = function(entry)
+                return {"", string.format("[item=%s] ", entry.name), entry.localised_name}
+            end,
+            tooltip = function(entry)
+                return entry.localised_description
+            end,
+            order = function(entry)
+                return entry.name
+            end,
+            constant_font = "default-bold"
+        },
+        {
+            name = "calories",
+            localised_name = {"sosciencity.calories"},
+            content = function(entry)
+                return {"sosciencity.value-with-unit", math.floor(entry.calories), {"sosciencity.kcal"}}
+            end,
+            order = function(entry)
+                return entry.calories
+            end
+        },
+        {
+            name = "fat",
+            localised_name = {"sosciencity.fat"},
+            content = function(entry)
+                return string.format("%.0f%%", 100 * entry.fat / (entry.fat + entry.carbohydrates + entry.proteins))
+            end,
+            order = function(entry)
+                return entry.fat / (entry.fat + entry.carbohydrates + entry.proteins)
+            end
+        },
+        {
+            name = "carbohydrates",
+            localised_name = {"sosciencity.carbohydrates"},
+            content = function(entry)
+                return string.format("%.0f%%", 100 * entry.carbohydrates / (entry.fat + entry.carbohydrates + entry.proteins))
+            end,
+            order = function(entry)
+                return entry.carbohydrates / (entry.fat + entry.carbohydrates + entry.proteins)
+            end
+        },
+        {
+            name = "proteins",
+            localised_name = {"sosciencity.proteins"},
+            content = function(entry)
+                return string.format("%.0f%%", 100 * entry.proteins / (entry.fat + entry.carbohydrates + entry.proteins))
+            end,
+            order = function(entry)
+                return entry.proteins / (entry.fat + entry.carbohydrates + entry.proteins)
+            end
+        },
+        {
+            name = "healthiness",
+            localised_name = {"sosciencity.health"},
+            content = function(entry)
+                return entry.healthiness
+            end,
+            order = function(entry)
+                return entry.healthiness
+            end
+        },
+        {
+            name = "luxury",
+            localised_name = {"sosciencity.luxury"},
+            content = function(entry)
+                return entry.luxury
+            end,
+            order = function(entry)
+                return entry.luxury
+            end
+        },
+        {
+            name = "taste",
+            localised_name = {"sosciencity.tastiness"},
+            content = function(entry)
+                return entry.taste_quality
+            end,
+            order = function(entry)
+                return entry.taste_quality
+            end
+        },
+        {
+            name = "taste-category",
+            localised_name = {"sosciencity.taste"},
+            content = function(entry)
+                return Food.taste_names[entry.taste_category]
+            end,
+            order = function(entry)
+                return entry.taste_category
+            end
+        }
     }
 }
 
