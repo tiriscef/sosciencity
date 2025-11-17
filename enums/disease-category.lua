b68082c2e90390b467087c8e613b1cbd90e4fe82
tiriscef <tiriscef@protonmail.com>
2025-11-17 16:23:17 +0100

    Disease system: Define Categories in their own table, some new categories

--- enums/disease-category.lua
+++ enums/disease-category.lua
@@ -2,13 +2,11 @@
 --- @enum DiseaseCategory
 local DiseaseCategory = {}
 
---XXX: DiseaseCategory and DiseasedCause likely could be merged
-
 --- consequences of bad health, distributed over time
 DiseaseCategory.health = 1
 --- consequences of bad sanity, distributed over time
 DiseaseCategory.sanity = 2
---- consequences of work related accidents, distributed over time
+--- non-work-related accidents, distributed over time
 DiseaseCategory.accident = 3
 --- consequences of genetic dispositions or complications during upbringing, distributed on birth
 DiseaseCategory.birth_defect = 4
@@ -20,5 +18,26 @@ DiseaseCategory.infection = 6
 DiseaseCategory.escalation = 7
 --- diseases that are a consequence of another disease's treatment
 DiseaseCategory.complication = 8
+--- diseases that are a consequence of not providing enough food
+DiseaseCategory.malnutrition = 9
+--- diseases that are a consequence of not providing enough drinking water
+DiseaseCategory.dehydration = 10
+--- diseases that are a consequence of providing unsafe food
+DiseaseCategory.food_poisoning = 11
+--- diseases that are a consequence of not providing enough drinking water
+DiseaseCategory.water_poisoning = 12
+
+-- Categories for work-related diseases
+
+--- Accidents/Diseases that occur in very demanding jobs
+DiseaseCategory.hard_work = 20
+--- Accidents/Diseases that occur in typical office jobs
+DiseaseCategory.office_work = 21
+--- Addidents/Diseases that occur in moderately demanding jobs
+DiseaseCategory.moderate_work = 22
+--- Accidents/Diseases that occur when working in the Fishing Hut
+DiseaseCategory.fishing_hut = 30
+--- Accidents/Diseases that occur when working in the Hunting/Gathering Hut
+DiseaseCategory.hunting_hut = 31
 
 return DiseaseCategory
--- enums/diseased-cause.lua
+++ enums/diseased-cause.lua
@@ -10,5 +10,8 @@ DiseasedCause.escalation = 5
 DiseasedCause.complication = 6
 DiseasedCause.birth = 7
 DiseasedCause.zoonosis = 8
+DiseasedCause.malnutrition = 9
+DiseasedCause.dehydration = 10
+DiseasedCause.workplace_accident = 11
 
 return DiseasedCause
