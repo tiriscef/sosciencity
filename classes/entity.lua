436326c5b5c11452862cf40c1bab28cd68309372
tiriscef <koun@gmx.de>
2025-12-08 16:03:56 +0100

    repaired the city combinator

--- classes/entity.lua
+++ classes/entity.lua
@@ -319,17 +319,40 @@ Register.set_entity_updater(Type.caste_education_building, update_caste_educatio
 ---------------------------------------------------------------------------------------------------
 -- << city combinator >>
 
-local function update_city_combinator(entry)
-    --[[local control_behavior = entry[EK.entity].get_control_behavior()
+local population_signals = {
+    ["signal-ember"] = Type.ember,
+    ["signal-orchid"] = Type.orchid,
+    ["signal-clockwork"] = Type.clockwork,
+    ["signal-gunfire"] = Type.gunfire,
+    ["signal-foundry"] = Type.foundry,
+    ["signal-gleam"] = Type.gleam,
+    ["signal-plasma"] = Type.plasma
+}
 
-    for type, signal in pairs(caste_signals) do
-        if Inhabitants.caste_is_researched(type) then
-            control_behavior.set_signal(type, {signal = signal, count = storage.population[type]})
+local function update_city_combinator(entry)
+    local control_behavior = entry[EK.entity].get_control_behavior()
+
+    for _, section in pairs(control_behavior.sections) do
+        for i = 1, 10 do
+            local signal = section.get_slot(i)
+            local value = signal.value
+            if not value then goto continue end
+
+            local name = value.name
+            if not name then goto continue end
+
+            local caste = population_signals[name]
+            if caste then
+                signal.min = storage.population[caste]
+                signal.max = storage.population[caste]
+                section.set_slot(i, signal)
+            end
+
+            ::continue::
         end
-    end]]
-    -- TODO: This doesn't work anymore and I currently have no clue how to implement this in factorio 2
+    end
 end
---Register.set_entity_updater(Type.city_combinator, update_city_combinator)
+Register.set_entity_updater(Type.city_combinator, update_city_combinator)
 
 ---------------------------------------------------------------------------------------------------
 -- << composting >>
