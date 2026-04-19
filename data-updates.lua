f393e9a2f9c0ae7709303de70b43b270a3a0f98c
tiriscef <tiriscef@protonmail.com>
2026-04-19 23:11:41 +0200

    shadowrealm the furniture unlock script because it's not working anymore

--- data-updates.lua
+++ data-updates.lua
@@ -9,7 +9,6 @@ require("datastage-scripts.garbage-to-landfill")
 require("datastage-scripts.item-descriptions")
 require("datastage-scripts.technology-descriptions")
 require("datastage-scripts.handcrafting")
-require("datastage-scripts.furniture-unlocks")
 require("datastage-scripts.placement-range-highlights")
 
 Tirislib.Prototype.finish()
