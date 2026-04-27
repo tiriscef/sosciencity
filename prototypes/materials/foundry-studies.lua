c9875acd81cb7a6182d9b6a1af0ad863d9b20ddd
tiriscef <tiriscef@protonmail.com>
2026-04-27 23:03:05 +0200

    foundry study chain

--- prototypes/materials/foundry-studies.lua
+++ prototypes/materials/foundry-studies.lua
@@ -17,18 +17,92 @@ Tirislib.Item.batch_create(
 ---------------------------------------------------------------------------------------------------
 -- << recipes >>
 
+-- Experimental Workshop
+
+Tirislib.RecipeGenerator.create_from_prototype {
+    results = {
+        {type = "item", name = "experimental-data", amount = 1}
+    },
+    ingredients = {
+        {type = "item", name = "tools", amount = 1},
+        {type = "item", name = "contraption", amount = 1}
+    },
+    category = "sosciencity-experimental-workshop",
+    energy_required = 6,
+    unlock = "foundry-caste"
+}
+
+Tirislib.RecipeGenerator.create_from_prototype {
+    results = {
+        {type = "item", name = "experimental-data", amount = 2}
+    },
+    ingredients = {
+        {type = "item", name = "power-tools", amount = 1},
+        {type = "item", name = "contraption", amount = 1},
+        {type = "item", name = "study-design", amount = 1}
+    },
+    category = "sosciencity-experimental-workshop",
+    energy_required = 8,
+    unlock = "academic-exchange"
+}
+
+-- Tech Institute
+
+Tirislib.RecipeGenerator.create_from_prototype {
+    results = {
+        {type = "item", name = "computing-model", amount = 1}
+    },
+    ingredients = {
+        {type = "item", name = "empty-hard-drive", amount = 1},
+        {type = "item", name = "paper", amount = 3}
+    },
+    category = "sosciencity-tech-institute",
+    energy_required = 8,
+    unlock = "foundry-caste"
+}
+
+-- Computing Center
+
+Tirislib.RecipeGenerator.create_from_prototype {
+    results = {
+        {type = "item", name = "complex-scientific-data", amount = 2}
+    },
+    ingredients = {
+        {type = "item", name = "experimental-data", amount = 2},
+        {type = "item", name = "computing-model", amount = 1}
+    },
+    category = "sosciencity-computing-center",
+    energy_required = 6,
+    unlock = "foundry-caste"
+}
+
 -- Foundry HQ
 
 Tirislib.RecipeGenerator.create_from_prototype {
     results = {
-        {type = "item", name = "complex-scientific-data", amount = 4}
+        {type = "item", name = "scientific-theory", amount = 1}
     },
     ingredients = {
-        {type = "item", name = "empty-hard-drive", amount = 4}
+        {type = "item", name = "complex-scientific-data", amount = 2},
+        {type = "item", name = "sketch", amount = 1}
     },
     category = "sosciencity-caste-foundry",
-    energy_required = 2,
+    energy_required = 8,
     unlock = "foundry-caste"
+}
+
+Tirislib.RecipeGenerator.create_from_prototype {
+    results = {
+        {type = "item", name = "scientific-theory", amount = 2}
+    },
+    ingredients = {
+        {type = "item", name = "complex-scientific-data", amount = 2},
+        {type = "item", name = "sketch", amount = 1},
+        {type = "item", name = "metastudy", amount = 1}
+    },
+    category = "sosciencity-caste-foundry",
+    energy_required = 10,
+    unlock = "academic-exchange"
 }
 
 Tirislib.RecipeGenerator.create_from_prototype {
---
+++ prototypes/technologies/foundry.lua
@@ -0,0 +1,18 @@
+
+Tirislib.Technology.create {
+    name = "academic-exchange",
+    icon = Tirislib.Prototype.placeholder_icon,
+    icon_size = 64,
+    prerequisites = {"foundry-caste", "gleam-caste"},
+    unit = {
+        count = 290,
+        ingredients = {
+            {"automation-science-pack", 1},
+            {"logistic-science-pack", 1},
+            {"chemical-science-pack", 1},
+            {"production-science-pack", 1},
+            {"utility-science-pack", 1}
+        },
+        time = 30
+    }
+}
