173a9017c4453739c2cd76ea9b223a56414269af
tirisabella <tiriscef@protonmail.com>
2026-05-12 11:20:39 +0200

    extracted all those category icons out of my precious data.lua

---
+++ icon-layer-config.lua
@@ -0,0 +1,41 @@
+Tirislib.BasePrototype.register_category_icon("handcrafting", {
+    path = "__sosciencity-graphics__/graphics/utility/hand.png",
+})
+Tirislib.BasePrototype.register_category_icon("workshop", {
+    path = "__sosciencity-graphics__/graphics/utility/tinkering.png",
+})
+Tirislib.BasePrototype.register_category_icon("enrichment", {
+    path = "__sosciencity-graphics__/graphics/icon/enrichment.png",
+    scale = 0.3,
+    no_tint = true,
+})
+Tirislib.BasePrototype.register_category_icon("pure-culture", {
+    path = "__sosciencity-graphics__/graphics/icon/pure-culture.png",
+    scale = 0.3,
+    no_tint = true,
+})
+Tirislib.BasePrototype.register_category_icon("farming", {
+    path = "__sosciencity-graphics__/graphics/icon/farming.png",
+    scale = 0.3,
+    no_tint = true,
+})
+Tirislib.BasePrototype.register_category_icon("plant-neogenesis", {
+    path = "__sosciencity-graphics__/graphics/icon/plant-neogenesis.png",
+    scale = 0.3,
+    no_tint = true,
+})
+Tirislib.BasePrototype.register_category_icon("slaughter", {
+    path = "__sosciencity-graphics__/graphics/icon/slaughter.png",
+    scale = 0.3,
+    tint = {r = 1, g = 0.2, b = 0.2},
+})
+Tirislib.BasePrototype.register_category_icon("breeding", {
+    path = "__sosciencity-graphics__/graphics/icon/breeding.png",
+    scale = 0.3,
+    no_tint = true,
+})
+Tirislib.BasePrototype.register_category_icon("keeping", {
+    path = "__sosciencity-graphics__/graphics/icon/keeping.png",
+    scale = 0.3,
+    no_tint = true,
+})
